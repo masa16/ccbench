911df3f3fed9bfa72db40aa32de4d84fec93f47b
Masahiro TANAKA <masa16.tanaka@gmail.com>
2020-10-14 19:00:38 +0900

    use compare_exchange_strong for LogQueue#enq

--- silo/include/log_queue.hh
+++ silo/include/log_queue.hh
@@ -2,11 +2,13 @@
 #include <mutex>
 #include <condition_variable>
 #include <queue>
+#include <atomic>
 
 class LogBuffer;
 
 class LogQueue {
 private:
+  std::atomic<unsigned int> my_mutex_;
   std::mutex mutex_;
   std::condition_variable cv_enq_;
   std::condition_variable cv_deq_;
@@ -14,13 +16,26 @@ private:
   std::size_t capacity_ = 1000;
   bool quit_ = false;
 
+private:
+  void my_lock() {
+    for (;;) {
+      unsigned int lock = 0;
+      if (my_mutex_.compare_exchange_strong(lock, 1)) return;
+      usleep(1);
+    }
+  }
+  void my_unlock() {
+    my_mutex_.store(0);
+  }
+
 public:
+  LogQueue() {my_mutex_.store(0);}
 
   void enq(LogBuffer* x) {
-    std::unique_lock<std::mutex> lock(mutex_);
-    cv_enq_.wait(lock, [this]{return queue_.size() < capacity_;});
+    my_lock();
     queue_.push(x);
     cv_deq_.notify_one();
+    my_unlock();
   }
 
   bool wait_deq() {
@@ -30,10 +45,10 @@ public:
   }
 
   LogBuffer *deq() {
-    std::unique_lock<std::mutex> lock(mutex_);
+    my_lock();
     auto ret = queue_.front();
     queue_.pop();
-    cv_enq_.notify_one();
+    my_unlock();
     return ret;
   }
 
