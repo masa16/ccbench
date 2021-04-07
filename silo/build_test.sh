e9c4afbd8dca859d8cc31891f52d02cf383178b6
Masahiro TANAKA <masa16.tanaka@gmail.com>
2021-04-07 20:09:33 +0900

    update build_test.sh

--- silo/build_test.sh
+++ silo/build_test.sh
@@ -7,9 +7,9 @@ esac
 c=$(grep '^processor' /proc/cpuinfo | wc -l)
 
 build() {
-  d=$1
+  local d=$1
   shift
-  opt="$*"
+  local opt="$*"
   rm -rf $d
   mkdir $d
   cd $d
@@ -18,12 +18,14 @@ build() {
   cd ..
 }
 
-build test_0log -D DURABLE_EPOCH=0
-build test_nlog -D DURABLE_EPOCH=1
-build test_nlog_deqmin -D DURABLE_EPOCH=1 -D DEQ_MIN_EPOCH=1
-build test_nlog_diff2 -D DURABLE_EPOCH=1 -D DEQ_MIN_EPOCH=1 -D MAX_EPOCH_DIFF=2
-build test_nlog_diff3 -D DURABLE_EPOCH=1 -D DEQ_MIN_EPOCH=1 -D MAX_EPOCH_DIFF=3
-build test_nlog_diff4 -D DURABLE_EPOCH=1 -D DEQ_MIN_EPOCH=1 -D MAX_EPOCH_DIFF=4
-#build test_0log_noidx -D DURABLE_EPOCH=0 -D MASSTREE_USE=0
-#build test_nlog_noidx -D DURABLE_EPOCH=1 -D MASSTREE_USE=0
-#build test_walpmem -D WAL=1 -D WALPMEM=0
+build test_0log -D DURABLE_EPOCH=0 &
+build test_nlog -D DURABLE_EPOCH=1 &
+build test_nlog_deqmin -D DURABLE_EPOCH=1 -D DEQ_MIN_EPOCH=1 &
+build test_nlog_diff1 -D DURABLE_EPOCH=1 -D DEQ_MIN_EPOCH=1 -D MAX_EPOCH_DIFF=1 &
+build test_nlog_diff2 -D DURABLE_EPOCH=1 -D DEQ_MIN_EPOCH=1 -D MAX_EPOCH_DIFF=2 &
+build test_nlog_diff3 -D DURABLE_EPOCH=1 -D DEQ_MIN_EPOCH=1 -D MAX_EPOCH_DIFF=3 &
+build test_nlog_diff4 -D DURABLE_EPOCH=1 -D DEQ_MIN_EPOCH=1 -D MAX_EPOCH_DIFF=4 &
+build test_0log_noidx -D MASSTREE_USE=0 -D DURABLE_EPOCH=0 -D DEQ_MIN_EPOCH=1 &
+build test_nlog_noidx -D MASSTREE_USE=0 -D DURABLE_EPOCH=1 -D DEQ_MIN_EPOCH=1 &
+#build test_walpmem -D WAL=1 -D WALPMEM=0 &
+wait
